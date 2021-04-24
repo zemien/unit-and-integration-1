fe2437a023e1fddac227efaf9dc67e9c6ad2e536
Shawn Lam <shawn@coderfrontline.com>
2021-04-25 06:54:30 +1200

    Refactored to use Theory where applicable

--- ShoppingCartServiceTests/ShippingCalculatorTest.cs
+++ ShoppingCartServiceTests/ShippingCalculatorTest.cs
@@ -1,4 +1,4 @@
-﻿using System;
+﻿using System.Collections.Generic;
 using ShoppingCartService.BusinessLogic;
 using ShoppingCartService.Models;
 using Xunit;
@@ -7,104 +7,27 @@ namespace ShoppingCartServiceTests
 {
     public class ShippingCalculatorTest
     {
-        [Fact]
-        public void StandardCustomerStandardShipping()
+
+        [InlineData(ShippingMethod.Standard, CustomerType.Standard, 100, 100)]
+        [InlineData(ShippingMethod.Expedited, CustomerType.Standard, 100, 120)]
+        [InlineData(ShippingMethod.Priority, CustomerType.Standard, 100, 200)]
+        [InlineData(ShippingMethod.Express, CustomerType.Standard, 100, 250)]
+        [InlineData(ShippingMethod.Standard, CustomerType.Premium, 100, 100)]
+        [InlineData(ShippingMethod.Expedited, CustomerType.Premium, 100, 100)]
+        [InlineData(ShippingMethod.Priority, CustomerType.Premium, 100, 100)]
+        [InlineData(ShippingMethod.Express, CustomerType.Premium, 100, 250)]
+        [Theory]
+        public void CalculateShippingMethodCost(ShippingMethod shippingMethod, CustomerType customerType, double baseCost, double shippingMethodCost)
         {
             var shippingCalculator = new ShippingCalculator();
-            var result = shippingCalculator.CalculateShippingMethodCost(
-                100,
-                ShoppingCartService.Models.ShippingMethod.Standard,
-                ShoppingCartService.Models.CustomerType.Standard);
+            var result = shippingCalculator.CalculateShippingMethodCost(baseCost, shippingMethod, customerType);
 
-            Assert.Equal(100, result);
+            Assert.Equal(shippingMethodCost, result);
         }
 
-        [Fact]
-        public void StandardCustomerExpeditedShipping()
-        {
-            var shippingCalculator = new ShippingCalculator();
-            var result = shippingCalculator.CalculateShippingMethodCost(
-                100,
-                ShoppingCartService.Models.ShippingMethod.Expedited,
-                ShoppingCartService.Models.CustomerType.Standard);
-
-            Assert.Equal(120, result);
-        }
-
-        [Fact]
-        public void StandardCustomerPriorityShipping()
-        {
-            var shippingCalculator = new ShippingCalculator();
-            var result = shippingCalculator.CalculateShippingMethodCost(
-                100,
-                ShoppingCartService.Models.ShippingMethod.Priority,
-                ShoppingCartService.Models.CustomerType.Standard);
-
-            Assert.Equal(200, result);
-        }
-
-        [Fact]
-        public void StandardCustomerExpressShipping()
-        {
-            var shippingCalculator = new ShippingCalculator();
-            var result = shippingCalculator.CalculateShippingMethodCost(
-                100,
-                ShoppingCartService.Models.ShippingMethod.Express,
-                ShoppingCartService.Models.CustomerType.Standard);
-
-            Assert.Equal(250, result);
-        }
-
-        [Fact]
-        public void PremiumCustomerStandardShipping()
-        {
-            var shippingCalculator = new ShippingCalculator();
-            var result = shippingCalculator.CalculateShippingMethodCost(
-                100,
-                ShoppingCartService.Models.ShippingMethod.Standard,
-                ShoppingCartService.Models.CustomerType.Premium);
-
-            Assert.Equal(100, result);
-        }
-
-        [Fact]
-        public void PremiumCustomerExpeditedShipping()
-        {
-            var shippingCalculator = new ShippingCalculator();
-            var result = shippingCalculator.CalculateShippingMethodCost(
-                100,
-                ShoppingCartService.Models.ShippingMethod.Expedited,
-                ShoppingCartService.Models.CustomerType.Premium);
-
-            Assert.Equal(100, result);
-        }
-
-        [Fact]
-        public void PremiumCustomerPriorityShipping()
-        {
-            var shippingCalculator = new ShippingCalculator();
-            var result = shippingCalculator.CalculateShippingMethodCost(
-                100,
-                ShoppingCartService.Models.ShippingMethod.Priority,
-                ShoppingCartService.Models.CustomerType.Premium);
-
-            Assert.Equal(100, result);
-        }
-
-        [Fact]
-        public void PremiumCustomerExpressShipping()
-        {
-            var shippingCalculator = new ShippingCalculator();
-            var result = shippingCalculator.CalculateShippingMethodCost(
-                100,
-                ShoppingCartService.Models.ShippingMethod.Express,
-                ShoppingCartService.Models.CustomerType.Premium);
-
-            Assert.Equal(250, result);
-        }
-
-        [Fact]
-        public void TravelCostForDifferentCountry()
+        [MemberData(nameof(CalculateTravelCostData))]
+        [Theory]
+        public void TravelCostForDifferentCountry(Address destination, uint numberOfItems, double travelCost)
         {
             var origin = new Address
             {
@@ -113,61 +36,49 @@ namespace ShoppingCartServiceTests
                 Street = "1234 left lane."
             };
 
-            var destination = new Address
-            {
-                Country = "NZ",
-                City = "Auckland",
-                Street = "1234 right lane."
-            };
+            var result = ShippingCalculator.CalculateTravelCost(origin, destination, numberOfItems);
 
-            var result = ShippingCalculator.CalculateTravelCost(origin, destination, 1);
-
-            Assert.Equal(15.0, result);
+            Assert.Equal(travelCost, result);
         }
 
-        [Fact]
-        public void TravelCostForDifferentCity()
+        public static List<object[]> CalculateTravelCostData()
         {
-            var origin = new Address
+            return new List<object[]>
             {
-                Country = "USA",
-                City = "Dallas",
-                Street = "1234 left lane."
+                new object[]
+                {
+                    new Address
+                    {
+                        Country = "NZ",
+                        City = "Auckland",
+                        Street = "1234 right lane."
+                    },
+                    1,
+                    15.0
+                },
+                new object[]
+                {
+                    new Address
+                    {
+                        Country = "USA",
+                        City = "New York",
+                        Street = "1234 right lane."
+                    },
+                    1,
+                    2.0
+                },
+                new object[]
+                {
+                    new Address
+                    {
+                        Country = "USA",
+                        City = "Dallas",
+                        Street = "1234 right lane."
+                    },
+                    1,
+                    1.0
+                },
             };
-
-            var destination = new Address
-            {
-                Country = "USA",
-                City = "New York",
-                Street = "1234 right lane."
-            };
-
-            var result = ShippingCalculator.CalculateTravelCost(origin, destination, 1);
-
-            Assert.Equal(2.0, result);
-        }
-
-
-        [Fact]
-        public void TravelCostForSameCity()
-        {
-            var origin = new Address
-            {
-                Country = "USA",
-                City = "Dallas",
-                Street = "1234 left lane."
-            };
-
-            var destination = new Address
-            {
-                Country = "USA",
-                City = "Dallas",
-                Street = "1234 right lane."
-            };
-
-            var result = ShippingCalculator.CalculateTravelCost(origin, destination, 1);
-
-            Assert.Equal(1.0, result);
         }
     }
 }
